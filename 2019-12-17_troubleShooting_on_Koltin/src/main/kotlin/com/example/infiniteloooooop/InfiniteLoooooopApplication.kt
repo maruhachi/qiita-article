119b1cabb4b051588082908011b70654c322ce8c
maruhachi <konchan1129@gmail.com>
2019-12-18 09:10:30 +0900

    ヒープを食うコード

---
+++ 2019-12-17_troubleShooting_on_Koltin/src/main/kotlin/com/example/infiniteloooooop/InfiniteLoooooopApplication.kt
@@ -0,0 +1,11 @@
+package com.example.infiniteloooooop
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InfiniteLoooooopApplication
+
+fun main(args: Array<String>) {
+	runApplication<InfiniteLoooooopApplication>(*args)
+}
---
+++ 2019-12-17_troubleShooting_on_Koltin/src/main/kotlin/com/example/infiniteloooooop/LoopController.kt
@@ -0,0 +1,33 @@
+package com.example.infiniteloooooop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.io.File
+import java.time.LocalDateTime
+
+@Controller
+class LoopController(
+        var mm: HashMap<String, String>
+) {
+
+    @ResponseBody
+    @GetMapping("/")
+    fun hello(): Map<String, String>{
+        return mapOf("hello" to "hell")
+    }
+
+    @ResponseBody
+    @GetMapping("/hell")
+    fun hell(): String{
+        val res = Oom()
+        mm.put(LocalDateTime.now().toString(), res)
+        return "ok?"
+    }
+
+    fun Oom(): String{
+        val inputStream = File("/Users/ykonno/test.txt").inputStream()
+        val reader = inputStream.bufferedReader()
+        return reader.readText()
+    }
+}
